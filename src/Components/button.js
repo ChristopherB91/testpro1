f9c227d659d449f55dc1dc7b348ca0d0a282a940
Chris <chrisbrowne91@gmail.com>
2022-08-12 16:20:23 -0400

    changing colors adding props

--- src/Components/button.js
+++ src/Components/button.js
@@ -1,8 +1,8 @@
 import * as React from 'react';
 import { Button } from '@mui/material/';
 
-function Button1() {
-    return <Button color='error' variant = 'contained'>Hello World</Button>
+function Button1(props) {
+    return <Button color='secondary' variant = 'contained'>{props.txt}</Button>
 }
 
 export default Button1
