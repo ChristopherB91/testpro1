f3622da9f129c3dc04de2865f9a33df83be56500
Chris <chrisbrowne91@gmail.com>
2022-08-12 01:51:49 -0400

    button component

---
+++ src/Components/button.js
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import { Button } from '@mui/material/';
+
+function Button1() {
+    return <Button variant = 'contained'>Hello World</Button>
+}
+
+export default Button1
