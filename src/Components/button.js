e80e3f3e41b0a513ff501d2f76ca0201782d2525
Chris <chrisbrowne91@gmail.com>
2022-08-12 15:28:17 -0400

    changed button color

--- src/Components/button.js
+++ src/Components/button.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { Button } from '@mui/material/';
 
 function Button1() {
-    return <Button variant = 'contained'>Hello World</Button>
+    return <Button color='error' variant = 'contained'>Hello World</Button>
 }
 
 export default Button1
