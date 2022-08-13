8d5ffbe0256ac925cb3738ac36ff5a2dddb1c2c5
Chris <chrisbrowne91@gmail.com>
2022-08-13 18:32:22 -0400

    Finished Navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,14 +1,23 @@
 import React from "react";
 import { Tab } from "@mui/material";
+import { Link } from "react-router-dom";
 // import Button1 from "./button";
 
 export default function Nav(props) {
   return (
-    <div>
+    <nav>
       {/* <Button1 color="secondary" variant="contained" txt="Hello everyone" /> */}
-      <Tab label={props.label0} />
-      <Tab label={props.label1} />
+    <Link to={props.label0}>
+    <Tab label={props.label0} />
+    </Link>
+    <Link to={props.label1}>
+    <Tab label={props.label1} />
+    </Link>
+      <Link to={props.label2}>
       <Tab label={props.label2} />
-    </div>
+    </Link>
+      
+      
+    </nav>
   );
 }
