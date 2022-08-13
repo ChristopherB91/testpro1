dc73773f899afbd6d9c37f681f099c452bb06158
Chris <chrisbrowne91@gmail.com>
2022-08-13 13:19:56 -0400

    header component

---
+++ src/Components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <img src={logo} alt="Shinobi logo" />
+      <p1>Welcome to Shinobi Counter</p1>
+      <p1>Where you can learn how to counter abilities in Shinob Striker</p1>
+    </div>
+  );
+}
+
+export default Home;
