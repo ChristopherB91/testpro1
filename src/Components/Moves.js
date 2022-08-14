ccedaa0388f89fb69314149b87b9f45a37242653
Chris <chrisbrowne91@gmail.com>
2022-08-14 19:21:56 -0400

    created a table and added data grid mui

--- src/Components/Moves.js
+++ src/Components/Moves.js
@@ -1,11 +1,34 @@
 import React from 'react'
+// import { DataGrid } from '@mui/x-data-grid';
 
 function Moves() {
     return (
         <div className='App-moves'>
-        Move: susanoo armor
-        Breakers: Hazan Striker,cooling breeze ninja tool, uchiha style halo dance.
+        Move: 
+        Breakers: r,cooling breeze ninja tool, uchiha style halo dance.
         images
+        <table>
+        <tr>
+          <th>Move</th>
+          <th>Breakers</th>
+          <th>Image of Breakers</th>
+        </tr>
+        <tr>
+          <td>susanoo armor</td>
+          <td>Cooling Breeze ninja tool, Hazan Strike, Uchiha Style halo dance</td>
+          <td>Male</td>
+        </tr>
+        <tr>
+          <td>Truth Seeker Orbs</td>
+          <td>Onyx Chidori, Toad Oil Bombs, Sage Art Rasenshuriken</td>
+          <td>Female</td>
+        </tr>
+        <tr>
+          <td>Subham</td>
+          <td>25</td>
+          <td>Male</td>
+        </tr>
+      </table>
         </div>
     )
 }
