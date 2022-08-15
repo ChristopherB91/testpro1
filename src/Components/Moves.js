9571422aa6903ac94eec3489ffbde1a7de14a2db
Chris <chrisbrowne91@gmail.com>
2022-08-15 17:18:03 -0400

    Centered table

--- src/Components/Moves.js
+++ src/Components/Moves.js
@@ -1,13 +1,22 @@
 import React from 'react'
-// import { DataGrid } from '@mui/x-data-grid';
+
+
+// const rows = GridRowsProp = [
+//     { id: 1, Move: 'susanoo armor', Breaker: 'Cooling Breeze ninja tool, Hazan Strike, Uchiha Style halo dance', Images: '' },
+//     { id: 2, Move: 'Truth Seeker Orbs', Breaker: 'Onyx Chidori, Toad Oil Bombs, Sage Art Rasenshuriken', Images: '' },
+//     { id: 3, Move: 'Time Space Hop', Breaker: 'Kamui Lightning Blade, Inferno Style: Flame Lightning, Youthful Roar', Images: '' }
+// ]
+
+// const columns = GridColDef = [
+//     { field: 'Move', headerName: 'Moves', width: 150},
+//     { field: 'Breaker', headerName: 'Breaker(s)', width: 150},
+//     { field: 'Images', headerName: 'Picture(s)', width: 150}
+// ]
 
 function Moves() {
     return (
         <div className='App-moves'>
-        Move: 
-        Breakers: r,cooling breeze ninja tool, uchiha style halo dance.
-        images
-        <table>
+        <table className='Moves-Table'>
         <tr>
           <th>Move</th>
           <th>Breakers</th>
@@ -24,8 +33,8 @@ function Moves() {
           <td>Female</td>
         </tr>
         <tr>
-          <td>Subham</td>
-          <td>25</td>
+          <td>Time Space Hop</td>
+          <td>Kamui Lightning Blade, Inferno Style: Flame Lightning, Youthful Roar</td>
           <td>Male</td>
         </tr>
       </table>
