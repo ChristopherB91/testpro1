cdca66fc449c6e9efdd75ee9fbc62c32e2ca17a2
Chris <chrisbrowne91@gmail.com>
2022-08-17 16:11:55 -0400

    new styles and fixed typo on homepage

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -7,7 +7,7 @@ function Home() {
     <div className="Home">
       <img src={logo} alt="Shinobi logo" />
       <p1>Welcome to Shinobi Counter</p1>
-      <p2><i>Where you can learn how to counter abilities in Shinob Striker</i></p2>
+      <p2><i>Where you can learn how to counter abilities in Shinobi Striker</i></p2>
       
     </div>
   );
